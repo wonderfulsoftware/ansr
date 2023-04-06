edee3a0536bb418b7f606b7dd49bf163075f9dfa
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-06 15:45:40 +0700

    ‍

--- web/src/Room.tsx
+++ web/src/Room.tsx
@@ -23,6 +23,7 @@ import {
 } from 'reactfire'
 import { ErrorAlert } from './ErrorAlert'
 import { UserName } from './UserName'
+import { FormGroup, FormHint } from './ui'
 
 export function Room() {
   const params = useParams()
@@ -647,28 +648,6 @@ function scoresToRankingEntry(scores: Record<string, number>) {
       score,
     }))
     .sort((a, b) => b.score - a.score)
-}
-
-export function FormGroup(props: FormGroup) {
-  return (
-    <div className="row">
-      <strong className={clsx('col-sm-3 col-form-label', props.pt0 && 'pt-0')}>
-        {props.label}
-      </strong>
-      <div className="col-sm-9">{props.children}</div>
-    </div>
-  )
-}
-
-export interface FormHint {
-  children?: ReactNode
-}
-export function FormHint(props: FormHint) {
-  return (
-    <div className="mt-1">
-      <small className="form-text text-muted">{props.children}</small>
-    </div>
-  )
 }
 
 export interface ActiveQuestionConnector {
