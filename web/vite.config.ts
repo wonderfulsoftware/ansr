2c11219dfef3282facbab62537dfcd7dc5d88dec
dtinth on MBP M1 <dtinth@spacet.me>
2023-07-16 15:27:21 +0700

    improve chunking

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/database'],
+        },
+      },
+    },
+  },
 })
